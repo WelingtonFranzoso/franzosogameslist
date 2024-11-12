e52816d21cfa50d12617be597e4534c8edc86a86
WelingtonFranzoso <welingtonfranzoso@gmail.com>
2024-11-12 17:36:47 -0300

    Project created

---
+++ src/main/java/com/franzoso/franzosogameslist/FranzosogameslistApplication.java
@@ -0,0 +1,13 @@
+package com.franzoso.franzosogameslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranzosogameslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranzosogameslistApplication.class, args);
+	}
+
+}
